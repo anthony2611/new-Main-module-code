32d651f2bc8f5b7fbbfaced78889262fa88aa06e
anthony <anthonyzimmer50@gmail.com>
2022-12-11 19:40:09 +0100

    added the 2 voids for handeling relays

---
+++ include/relay.h
@@ -0,0 +1,83 @@
+//setup the relays
+#ifdef relay
+
+    void relay_setup() {
+        #ifdef relay1
+            pinMode(relay1, OUTPUT);
+            mqtt_client.subscribe(mqtt_topic_relay1);
+        #endif
+        #ifdef relay2
+            pinMode(relay2, OUTPUT);
+            mqtt_client.subscribe(mqtt_topic_relay2);
+        #endif
+        #ifdef relay3
+            pinMode(relay3, OUTPUT);
+            mqtt_client.subscribe(mqtt_topic_relay3);
+        #endif
+        #ifdef relay4
+            pinMode(relay4, OUTPUT);
+            mqtt_client.subscribe(mqtt_topic_relay4);
+        #endif
+    }
+
+    //loop for the relays
+    void relay_loop() {
+        #ifdef relay1
+        if (mqtt_topic == mqtt_topic_relay1) {
+            if (mqtt_payload == "true") {
+            digitalWrite(relay1, HIGH);
+            Serial.println("relay1 on");
+            }
+            else if (mqtt_payload == "false") {
+            digitalWrite(relay1, LOW);
+            Serial.println("relay1 off");
+            }
+            mqtt_topic = "";
+            mqtt_payload = "";
+        }
+        #endif
+        #ifdef relay2
+        if (mqtt_topic == mqtt_topic_relay2) {
+            if (mqtt_payload == "true") {
+            digitalWrite(relay2, HIGH);
+            Serial.println("relay2 on");
+            }
+            else if (mqtt_payload == "false") {
+            digitalWrite(relay2, LOW);
+            Serial.println("relay2 off");
+            }
+            mqtt_topic = "";
+            mqtt_payload = "";
+        }
+        #endif
+        #ifdef relay3
+        if (mqtt_topic == mqtt_topic_relay3) {
+            if (mqtt_payload == "true") {
+            digitalWrite(relay3, HIGH);
+            Serial.println("relay3 on");
+            }
+            else if (mqtt_payload == "false") {
+            digitalWrite(relay3, LOW);
+            Serial.println("relay3 off");
+            }
+            mqtt_topic = "";
+            mqtt_payload = "";
+        }
+        #endif
+        #ifdef relay4
+        if (mqtt_topic == mqtt_topic_relay4) {
+            if (mqtt_payload == "true") {
+            digitalWrite(relay4, HIGH);
+            Serial.println("relay4 on");
+            }
+            else if (mqtt_payload == "false") {
+            digitalWrite(relay4, LOW);
+            Serial.println("relay4 off");
+            }
+            mqtt_topic = "";
+            mqtt_payload = "";
+        }
+        #endif
+    }
+
+#endif
