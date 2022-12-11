08b376a9a34944817766035e8714d67dd1e50998
anthony <anthonyzimmer50@gmail.com>
2022-12-11 19:39:14 +0100

    added relays

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 //################################################# LIBRARIES ##############################################################
-//do not change the libraries
+//do not change the libraries!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   #include <Arduino.h>
   #include <ESP8266WiFi.h>
   #include <PubSubClient.h>
@@ -33,31 +33,40 @@
   #define mqtt_port 1883                      //mqtt port
   char* device_name = "XXX_ESP8266";         //device name PLEASE CHANGE 
   String main_topic = "sensor/";              //the main topic for all devices
+  String relay_tpoic = "relays/";              //the relay topic for the relays if defined
+
 
 //please config the sensors you whant to have
+//uncomment the sensors you have and set the pin number for the sensors and unncomment the sensor topic
   #define dallas_temp                         //dell temperature sensor if you have one 
         #define dallas_temp_pin  2                //dell fan sensor if you have one 
   #define LDR                                 //LDR sensor if you have one
         #define LDR_pin A0                        //LDR pin (analog pin / ADC)
   #define motion_detector                     //motion detector if you have one
         #define motion_detector_pin  3            //motion detector pin
-  //#define relay                            //not yet implemented
-        //#define relay1
-        //#define relay2
-        //#define relay3
-        //#define relay4
+
+//please config the actuators you whant to have
+ #define relay                            //relay if you have one or more uncomment the relays you have and set the pin number for the relays and unncomment the relay topic
+        #define relay1 4                   //relay 1 pin (digital pin)
+ //       #define relay2 5                   //relay 2 pin (digital pin)
+ //       #define relay3 6                   //relay 3 pin (digital pin)
+ //       #define relay4 7                   //relay 4 pin (digital pin)
 
 
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!DO NOT CHANGE anything below this line unless you know what you are doing!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 //################################################# Libraries to setup and some string gerneating ##############################
 
-//DO NOT CHANGE anything below this line unless you know what you are doing
+
   //setup for wifi and mqtt
     WiFiClient espClient;                 //create the wifi object
     PubSubClient mqtt_client(espClient);  //create the mqtt client object
     bool send_data = false;               //send data is for the moduls to now when to send data to the server 
     int connect_attempts = 0;             //mqtt connect attemps at first run
     #define mqtt_client_id device_name    //set mqtt client id (normaly the device name)
+    String mqtt_topic;
+    String mqtt_payload;
 
     //gereating the mqtt_device_topic_string and any other mqtt stuff
     String mqtt_device_topic_string = main_topic + device_name;           //device topic as string if needed
@@ -95,6 +104,28 @@
       #include <motion_detector.h>
     #endif
     #ifdef relay
+      //topic for the relays
+      String mqtt_topic_relay_string = relay_tpoic + device_name;           //device topic as string if needed
+      const char* mqtt_topic_relay = mqtt_topic_relay_string.c_str();       //device topic as char* if needed
+
+      //making topic for eche defined relay
+      #ifdef relay1
+        String mqtt_topic_relay1_string = mqtt_topic_relay_string + "/relay1";   //constructing the relay1 string
+        const char* mqtt_topic_relay1 = mqtt_topic_relay1_string.c_str();   //converting relay1 string to a char*
+      #endif
+      #ifdef relay2
+        String mqtt_topic_relay2_string = mqtt_topic_relay_string + "/relay2";   //constructing the relay2 string
+        const char* mqtt_topic_relay2 = mqtt_topic_relay2_string.c_str();   //converting relay2 string to a char*
+      #endif
+      #ifdef relay3
+        String mqtt_topic_relay3_string = mqtt_topic_relay_string + "/relay3";   //constructing the relay3 string
+        const char* mqtt_topic_relay3 = mqtt_topic_relay3_string.c_str();   //converting relay3 string to a char*
+      #endif
+      #ifdef relay4
+        String mqtt_topic_relay4_string = mqtt_topic_relay_string + "/relay4";   //constructing the relay4 string
+        const char* mqtt_topic_relay4 = mqtt_topic_relay4_string.c_str();   //converting relay4 string to a char*
+      #endif
+      //includs for the relays
       #include <relay.h>
     #endif
 
@@ -108,9 +139,15 @@
     PubSubClient_setup();
     PubSubClient_first_connet();
 
+    //setup for the motion detection if enabled
     #ifdef motion_detector
       motion_detector_setup(motion_detector_pin);
-    #endif  
+    #endif 
+    //setup for the relays if enabled 
+    #ifdef relay
+      relay_setup();
+    #endif
+
   }
 
   void loop() {
@@ -127,6 +164,9 @@
     #endif
     #ifdef motion_detector
       motino_detector_loop(motion_detector_pin,send_data);
+    #endif
+    #ifdef relay
+      relay_loop();
     #endif
 
 
